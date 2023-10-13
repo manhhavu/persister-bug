493bda4a71c837ab3b9044017d758930f2b28ba7
manhhavu <manhha@htoh.io>
2023-10-13 09:14:49 +0200

    Reproduce suspense bug of PersistQueryClientProvider

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Suspense } from 'react';
+import Checkbox from 'expo-checkbox';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import { useSuspenseQuery, QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createAsyncStoragePersister } from '@tanstack/query-async-storage-persister'
+import axios from 'axios'
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PersistQueryClientProvider
+      persistOptions={{ persister: persister }}
+      client={queryClient}
+    >
+      <View style={styles.container}>
+        <Suspense fallback={<ActivityIndicator />}>
+          <Todo />
+        </Suspense>
+      </View>
+    </PersistQueryClientProvider>
   );
 }
+
+const queryClient = new QueryClient({})
+
+const persister = createAsyncStoragePersister({
+  storage: AsyncStorage,
+})
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +35,33 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'column',
   },
 });
+
+function Todo() {
+  const { data: todo } = useTodo()
+  return (
+    <View style={{flexDirection: 'row'}}>
+      <Checkbox
+          value={todo.completed}
+          style={{alignSelf: 'center'}}
+        />
+      <Text style={{marginLeft: 8}}>{todo.title}</Text>
+    </View>
+  )
+}
+
+function useTodo() {
+  return useSuspenseQuery({
+    queryKey: ['todo'],
+    queryFn: async () => {
+      return await fetchTodo()
+    }
+  })
+}
+
+async function fetchTodo() {
+  const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+  return response.data
+}
